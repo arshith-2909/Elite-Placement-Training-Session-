4cefa5513c20d73648c00b94be310bb8cc3fd1b2
arshith-2909 <arshith2909@gmail.com>
2026-01-17 21:47:19 +0530

    Day 3 Practise session

--- Trees/TreeConstruction.cpp
+++ Trees/TreeConstruction.cpp
@@ -44,6 +44,69 @@ void postorder(Tree* root)
     cout<< root->data<<" ";
 }
 
+void levelOrder(Tree* root)
+{
+    if(root == nullptr)
+        return;
+    queue<Tree*> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        Tree* node = q.front();
+        cout<<node->data<<" ";
+        q.pop();
+        if(node->left)
+            q.push(node->left);
+        if(node->right)
+            q.push(node->right);
+    }
+
+}
+
+Tree* insertKey(Tree* root, int key)
+{
+    if(root == nullptr)
+    {
+        Tree* newNode = new Tree(key);
+        return newNode;
+    }
+    queue<Tree*> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        Tree* temp = q.front();
+        q.pop();
+        if(!temp->left)
+        {
+            temp->left = new Tree(key);
+            break;
+        }
+        else
+            q.push(temp->left);
+        if(!temp->right)
+        {
+            temp->right = new Tree(key);
+            break;
+        }
+        else
+            q.push(temp->right);
+    }
+    return root;
+}
+
+bool searchKey(Tree* root, int key)
+{
+    if(root == nullptr)
+        return false;
+    if(root->data == key)
+    {
+        return true;
+    }
+    bool leftsearch = searchKey(root->left, key);
+    bool rightsearch = searchKey(root->right, key);
+    return leftsearch || rightsearch;
+}
+
 int main()
 {
     Tree* root = new Tree(1);
@@ -53,11 +116,23 @@ int main()
     root->left->right = new Tree(8);
     root->right->left = new Tree(2);
     root->right->right = new Tree(4);
+    int key;
+    // cout<<"Insert a Key : ";
+    // cin>>key;
+    // insertKey(root, key);
     cout<<"Inorder Traversal : ";
     inorder(root);
     cout<<endl<<"Preorder Traversal : ";
     preorder(root);
     cout<<endl<<"Postorder Traversal : ";
     postorder(root);
+    cout<<endl<<"Levelorder Traversal : ";
+    levelOrder(root);
+    cout<<endl<<"Enter the value to be searched : ";
+    cin>>key;
+    if(searchKey(root, key))
+        cout<<key<<" found.";
+    else
+        cout<<key<<" not found.";
     return 0;
 }
